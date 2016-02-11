3244eb29e9ec3aeb642837b8d161b6e88dc95a57
Olaf Veerman <olaf.veerman@gmail.com>
2016-02-11 12:11:33 -0500

    Add a tool to map an area name to an ISO code

---
+++ worker/tools/map-iso.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// Maps a string with the name of an administrative area, and maps it to the
+// ISO code of the district / county it belongs to.
+
+var _ = require('lodash');
+var areaMap = require('./areas-map.json');
+
+module.exports = function (dirtyName) {
+  var matchedArea = _.find(areaMap, function(o) { return clean(o.name) == clean(dirtyName); });
+  var mappedIso = (matchedArea) ? (matchedArea.iso || matchedArea.parent_iso) : undefined;  
+
+  return mappedIso;
+}
+
+function clean(dirtyName) {
+  var cleanerName = dirtyName.toLowerCase();
+  cleanerName = cleanerName.trim();
+  cleanerName = cleanerName.replace('_', ' ');
+
+  return cleanerName;
+}
