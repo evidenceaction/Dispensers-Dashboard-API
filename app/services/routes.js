f1343f38f556a9b59e1653fc7269191c25efbbd2
Daniel da Silva <daniel@developmentseed.org>
2016-03-03 11:53:44 +0000

    Remove unneeded files

--- app/services/routes.js
+++ app/services/routes.js
@@ -1,11 +1,9 @@
 'use strict';
 var base = require('../controllers/base');
-var country = require('../controllers/country');
 var kpi = require('../controllers/kpi');
 
 module.exports = [
   { method: 'GET', path: '/', config: base.index },
-  { method: 'GET', path: '/country/{country}', config: country.countryInfo },
   { method: 'GET', path: '/kpi/access', config: kpi.access },
   { method: 'GET', path: '/kpi/usage', config: kpi.usage },
   { method: 'GET', path: '/kpi/reliability', config: kpi.reliability },
