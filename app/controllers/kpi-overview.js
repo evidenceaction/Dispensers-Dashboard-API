8fe0e076533af200911c61c8239fc0f0d1a61f14
Olaf Veerman <olaf.veerman@gmail.com>
2016-03-17 07:51:56 -0400

    Make numbers numeric

--- app/controllers/kpi-overview.js
+++ app/controllers/kpi-overview.js
@@ -2,13 +2,13 @@
 var boom = require('boom');
 var config = require('../config');
 var dataLoader = require('../utils/yaml-md-loader');
-var carbonData = require('../data/overview-data.json');
+var overviewData = require('../data/overview-data.json');
 
 module.exports = {
   handler: (request, reply) => {
     let contentP = dataLoader(`${config.baseDir}/content/section-overview-home.md`);
 
-    Promise.all([carbonData, contentP])
+    Promise.all([overviewData, contentP])
       .then(res => {
         res[0].content = res[1];
         reply(res[0]);
