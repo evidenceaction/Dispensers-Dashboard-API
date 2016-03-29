4579303ceff944637ddee65e5c29dd8f010f539e
Olaf Veerman <olaf.veerman@gmail.com>
2016-03-29 10:16:15 -0400

    Abstract usage calculation

--- app/utils/data-utils.js
+++ app/utils/data-utils.js
@@ -24,3 +24,48 @@ module.exports.parseCountry = function (country) {
     return [1, 2, 3];
   }
 };
+
+// ######################################################################
+// Calculate the use rate
+
+// Notes on general method.
+// Sampling (1.5%) is done by program, ~8 households measured per waterpoint
+// Any reading > 0 is a positive reading
+//
+// Calculate:
+// The sql query return the avg adoption rate per program-year-month.
+// It also provides the total amount of dispensers.
+//
+// The next code calculates the avg adoption rate per timestep
+// weighing each program's avg by total dispensers installed.
+
+module.exports.useRate = function (results, startDate) {
+  let finalValues = [];
+  _(results)
+    .filter(o => o.timestep >= startDate && o.timestep < moment.utc().startOf('month'))
+    .groupBy('timestep')
+    .forEach((o, tsI) => {
+      // Total dispensers per time period.
+      let totalXPeriod = 0;
+      // Avg dispensers in use per time period.
+      let inUseXPeriod = 0;
+      _.forEach(o, d => {
+        // A null means that there's no reading and shouldn't be taken into
+        // account (it's not a 0)
+        if (d.avg_dispenser !== null) {
+          inUseXPeriod += d.avg_dispenser;
+          totalXPeriod += d.dispensers_total;
+        }
+      });
+
+      finalValues.push({
+        timestep: moment.utc(tsI).format('YYYY-MM-DD'),
+        tcr_avg: inUseXPeriod / totalXPeriod * 100,
+        raw: {
+          readings: inUseXPeriod,
+          dis_total: totalXPeriod
+        }
+      });
+    });
+  return finalValues;
+};
