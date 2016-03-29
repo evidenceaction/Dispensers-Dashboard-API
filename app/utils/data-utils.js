4a975c5245737f3433443c855e42dd420af9404d
Olaf Veerman <olaf.veerman@gmail.com>
2016-03-29 10:16:35 -0400

    Remove obsolete function

--- app/utils/data-utils.js
+++ app/utils/data-utils.js
@@ -1,12 +1,6 @@
 'use strict';
 var _ = require('lodash');
 
-// Sum data for a particular field, prior to given startDate
-module.exports.sumOldData = function (allData, field, startDate) {
-  let oldData = _.filter(allData, o => o.timestep < startDate);
-  return _.sumBy(oldData, field);
-};
-
 // Parse country parameter
 module.exports.parseCountry = function (country) {
   if (country) {
