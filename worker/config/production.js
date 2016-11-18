299d533a4fc77b390d7d80a48fe7192e838b2373
Leah Ndikuwera <lndiku@users.noreply.github.com>
2016-11-18 18:00:51 +0300

    Update production.js

--- worker/config/production.js
+++ worker/config/production.js
@@ -4,6 +4,7 @@
 module.exports = {
   environment: 'production',
   sourceDb: {
+    url: '52.58.189.131',
     host: 'mysql-db.cnltbg2tkrfl.eu-central-1.rds.amazonaws.com',
     user: 'devseed',
     password: 'seedguest73',
