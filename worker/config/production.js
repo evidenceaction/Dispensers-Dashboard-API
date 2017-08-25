e69a32a591de5926b0049b9a2e2e7b7867735db6
Olaf Veerman <olaf.veerman@gmail.com>
2017-08-25 08:26:48 -0400

    Clean up config

--- worker/config/production.js
+++ worker/config/production.js
@@ -4,10 +4,9 @@
 module.exports = {
   environment: 'production',
   sourceDb: {
-    url: '52.58.189.131',
-    host: 'mysql-db.cnltbg2tkrfl.eu-central-1.rds.amazonaws.com',
-    user: 'devseed',
-    password: 'seedguest73',
-    database: 'evidence_action_dsw'
+    host: null,
+    user: null,
+    password: null,
+    database: null
   }
 };
